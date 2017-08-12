61d8f5e24445f099fbf1fb2bdc77c30b83197a98
sunshine-lin <you@example.com>
2017-08-12 10:43:33 +0800

    mintUI配置完毕

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+html {
+  font-size: 13.3333333vw;
+}
+html,body,#app {
+  width: 100%;
+  height: 100%;
+}
+body {
+  background: #f4f4f4;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+      <mt-header fixed title="固定在顶部">
+        <router-link to="/" slot="left">
+          <mt-button icon="back">返回</mt-button>
+        </router-link>
+        <mt-button icon="more" slot="right"></mt-button>
+      </mt-header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
